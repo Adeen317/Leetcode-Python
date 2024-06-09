325e2944f76bb5d6f22d93d58b8f8698a2d86157
Muhammad Adeen <Adeen317>
2024-06-09 18:33:23 +0500

    hashtable-string

---
+++ Random Questions/Rotate Image.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+    
+        # Transpose the matrix
+        for i in range(n):
+            for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        
+        # Reverse each row
+        for i in range(n):
+            matrix[i].reverse()
+            
