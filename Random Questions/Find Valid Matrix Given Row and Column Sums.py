ad0d7a70a03284fa7d51e1ead8ccf45ce1c5bdb6
Muhammad Adeen <Adeen317>
2024-07-21 02:38:35 +0500

    array greedy matrix

---
+++ Random Questions/Find Valid Matrix Given Row and Column Sums.py
@@ -0,0 +1,22 @@
+class Solution:
+    def restoreMatrix(self, rowSum: List[int], colSum: List[int]) -> List[List[int]]:
+        ROWS, COLS=len(rowSum), len(colSum)
+        res=[[0]* COLS for _ in range(ROWS)]
+        for r in range(ROWS):
+            res[r][0]=rowSum[r]
+        
+        for c in range(COLS):
+            cur_col_sum = 0
+            for r in range(ROWS):
+                cur_col_sum += res[r][c]
+
+            r=0
+            while cur_col_sum > colSum[c]:
+                diff=cur_col_sum-colSum[c]
+                max_shift = min(res[r][c], diff)
+                res[r][c] -= max_shift
+                res[r][c+1] += max_shift
+                cur_col_sum -= max_shift
+                r+=1
+
+        return res 
