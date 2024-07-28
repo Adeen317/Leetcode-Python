85fbb8a20abbceefe23e89d30d0632000692293b
Muhammad Adeen <Adeen317>
2024-07-29 03:15:03 +0500

    BFS-Graph

---
+++ Random Questions/Second minimum time to reach destination.py
@@ -0,0 +1,31 @@
+class Solution:
+    def secondMinimum(self, n: int, edges: List[List[int]], time: int, change: int) -> int:
+        adj = defaultdict(list)
+        for v1, v2 in edges:
+            adj[v1].append(v2)
+            adj[v2].append(v1) 
+
+        q = deque([1])
+        cur_time = 0
+        res = -1
+
+        visit_time = defaultdict(list)
+
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node == n:
+                    if res != -1:
+                        return cur_time
+                    res = cur_time
+                for nei in adj[node]:
+                    nei_times = visit_time[nei]
+                    if len(nei_times) == 0 or len(nei_times) == 1 and nei_times[0] != cur_time:
+                        q.append(nei)
+                        nei_times.append(cur_time)
+            
+            if (cur_time // change) % 2 == 1:
+                cur_time += change - (cur_time % change)
+            cur_time += time
+
+                
